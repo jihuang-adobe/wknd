50c434f5bf5ec1653d30a6eb71b926cf1e340605
Jian Huang <jihuang@adobe.com>
2024-06-20 16:42:34 -0400

    work in progress

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -2,9 +2,15 @@ export default function decorate(block) {
   const buttons = document.createElement('div');
   buttons.className = 'carousel-buttons';
   [...block.children].forEach((row, i) => {
-    const classes = ['image', 'text'];
-    classes.forEach((e, j) => {
-      row.children[j].classList.add(`carousel-${e}`);
+    console.log(row);
+    console.log(i);
+    const totalColumnCount = [...row.children].length;
+    [...row.children].forEach((column, columnIndex) => {
+      if(columnIndex+1 != totalColumnCount) {
+        column.classList.add(`carousel-image`);
+      } else {
+        column.classList.add(`carousel-text`);
+      }
     });
     /* buttons */
     const button = document.createElement('button');
